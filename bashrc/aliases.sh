2d8fa52af35590c19b4f05476f0abef2d5832759
Maksym Khavil <khavimak@fit.cvut.cz>
2024-02-06 11:33:05 +0100

    [brc] add directory-grouping to ls alias

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -4,7 +4,7 @@ mkcd (){
 	mkdir -p -- "$1" && cd -P -- "$1"
 }
 
-alias ls='ls --color=auto'
+alias ls='ls --color=auto --group-directories-first'
 alias ll='ls -la'
 alias grep='grep --color=auto'
 alias batt='cbatticon'
