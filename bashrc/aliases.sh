e7fd8af35c911a720c80e203632d73350b42bf8f
Maksym Khavil <khavimak@fit.cvut.cz>
2024-02-06 11:19:15 +0100

    [brc] add view alias

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -8,5 +8,6 @@ alias ls='ls --color=auto'
 alias ll='ls -la'
 alias grep='grep --color=auto'
 alias batt='cbatticon'
+alias view='vim -R'
 alias aweconfig="$EDITOR ~/.config/awesome/rc.lua"
 alias awetheme="$EDITOR ~/.config/awesome/mytheme.lua"
