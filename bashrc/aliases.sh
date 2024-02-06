be5074c95ad8be8914b061508f431d1fedf6705b
Maksym Khavil <khavimak@fit.cvut.cz>
2024-02-06 11:14:08 +0100

    [brc] add aliasing script

---
+++ bashrc/aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias batt='cbatticon'
+alias aweconfig="$EDITOR ~/.config/awesome/rc.lua"
+alias awetheme="$EDITOR ~/.config/awesome/mytheme.lua"
