8c03e8df89107562be5cb26d62a6ebbcf97e84ed
Maksym Khavil <khavimak@fit.cvut.cz>
2024-02-09 14:58:16 +0100

    [brc] add cpp long compilation alias

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -4,6 +4,7 @@ mkcd (){
 	mkdir -p -- "$1" && cd -P -- "$1"
 }
 
+alias gll='g++ -std=c++20 -Wall -pedantic -Wno-long-long'
 alias ls='ls --color=auto --group-directories-first'
 alias ll='ls -la'
 alias grep='grep --color=auto'
