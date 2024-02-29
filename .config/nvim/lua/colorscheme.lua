d41b7250b86897cfc3c647af00af1068ebe1140b
Maksym Khavil <khavimak@fit.cvut.cz>
2024-02-29 21:05:37 +0100

    [vim] concretise colorscheme subtype

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,5 +1,7 @@
 -- Set the colorscheme to sonokai using a protected call
 -- in case it isn't installed
+vim.cmd("let g:sonokai_style = 'andromeda'")
+vim.cmd("let g:sonokai_better_performance = 1")
 local status, _ = pcall(vim.cmd, "colorscheme sonokai")
 if not status then
     -- Print an error message if the colorscheme is not installed
