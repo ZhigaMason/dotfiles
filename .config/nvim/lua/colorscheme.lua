b481bd0067597821ebdab2ff5516a7e014c965a8
Maksym Khavil <khavimak@fit.cvut.cz>
2024-02-29 21:00:06 +0100

    [vim] add colorscheme file

---
+++ .config/nvim/lua/colorscheme.lua
@@ -0,0 +1,8 @@
+-- Set the colorscheme to sonokai using a protected call
+-- in case it isn't installed
+local status, _ = pcall(vim.cmd, "colorscheme sonokai")
+if not status then
+    -- Print an error message if the colorscheme is not installed
+    print("Colorscheme not found!") 
+    return
+end
